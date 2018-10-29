dfa716fe2fc065785187583a7480d5607ee08644
yingjh <yjh.1234@qq.com>
2018-10-29 17:12:55 +0800

    添加新的依赖

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
+
+const Index = () => import('../pages/Index.vue')
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Index',
+      component: Index
     }
   ]
 })
