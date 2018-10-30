415f43a690bd415e2600c2a798ca3629f02223c0
yingjh <yjh.1234@qq.com>
2018-10-30 17:31:09 +0800

    修改顶部栏

--- src/components/MHead.vue
+++ src/components/MHead.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="tabbar-theme white flex-row align-center" id="top-nav">
-    <div></div>
+    <span></span>
     <div></div>
     <div>登录</div>
   </div>
@@ -16,5 +16,17 @@ export default {
   #top-nav {
     height: 44px;
     font-size: 14px;
+    position: sticky;
+    top: 0;
+    left: 0;
+    z-index: 999;
+
+    & > span {
+      width: 20px;
+      height: 18px;
+      margin: 0 0 0 15px;
+      background: url(../assets/common/bars.png) no-repeat;
+      background-size: 100% 100%;
+    }
   }
 </style>
