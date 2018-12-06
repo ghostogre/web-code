adfb1c92f8ab71ca2406f50121969a7df7384462
yingjh <yjh.1234@qq.com>
2018-12-06 17:37:07 +0800

    去除有问题的组件

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,22 +1,11 @@
 <template>
-  <m-scroll class="column">
-    <m-head></m-head>
-    <div id="slide-wrapper" class="column">
-      <slide-card class="slide"></slide-card>
-    </div>
-  </m-scroll>
+  <div></div>
 </template>
 
 <script>
-import MHead from '../components/MHead.vue'
-import MScroll from '../components/MScroll.vue'
-import SlideCard from '../components/SlideCard.vue'
 export default {
   name: 'Index',
   components: {
-    MHead,
-    MScroll,
-    SlideCard
   },
   methods: {
   }
@@ -24,15 +13,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  #slide-wrapper {
-    width: 100%;
-    height: 450px;
-    overflow: hidden;
-    justify-content: center;
-    background-color: #565f77;
-  }
-  .slide {
-    width: 320px;
-    height: 320px;
-  }
+
 </style>
